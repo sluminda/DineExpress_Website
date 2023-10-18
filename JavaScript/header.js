beae970217cb9fbe7a69a6dc2da345ded0b2ea9c
Luminda Saranga <sluminda@gmail.com>
2023-10-18 18:17:16 +0530

    Search Bar Updated

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -16,3 +16,42 @@ toggleBtn.onclick = function (){
     : 'fa-solid fa-bars'
 }
 
+
+
+let isSearchVisible = false; // Track the visibility state
+
+function onButtonClick() {
+    const textBox = document.getElementById('textInput');
+
+    if (!isSearchVisible) {
+        textBox.classList.remove('hide');
+        textBox.classList.add('show');
+        textBox.style.width = '200px'; // Adjust the width as needed
+        textBox.focus();
+        textBox.classList.add('transition-placeholder'); // Add the class for the placeholder transition
+        isSearchVisible = true;
+    }
+}
+
+function onInputBlur() {
+    const textBox = document.getElementById('textInput');
+
+    if (isSearchVisible) {
+        textBox.classList.remove('show');
+        textBox.classList.add('hide');
+        textBox.style.width = '0'; // Set the width back to 0 when hiding
+        textBox.classList.remove('transition-placeholder'); // Remove the class to reset the placeholder
+        isSearchVisible = false;
+    }
+}
+
+
+
+
+
+
+// function oniButtonClick() {
+//     const textBox = document.getElementById('textInput');
+//     textBox.classList.remove('show');
+//     textBox.classList.add('hide');
+// }
